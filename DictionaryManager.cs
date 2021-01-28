bf67ba76f14db3fdd8ebe0507a4111d9ea058b1e
EnderCanakci <ender.eyv@hotmail.com>
2021-01-28 16:14:48 +0300

    Add project files.

---
+++ DictionaryManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class DictionaryManager<K, V>
+    {
+        K[] keys;
+        V[] values;
+
+        public DictionaryManager()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+        public void Add(K key, V value)
+        {
+            K[] tempArrayKeys = keys;
+            V[] tempArrayValues = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempArrayKeys.Length; i++)
+            {
+                keys[i] = tempArrayKeys[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            for (int a = 0; a < tempArrayValues.Length; a++)
+            {
+                values[a] = tempArrayValues[a];
+            }
+            values[values.Length - 1] = value;
+        }
+        public void Show()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " = " + values[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryManager<int, string> X = new DictionaryManager<int, string>();
+            X.Add(10, "Taha");
+            X.Add(20, "Ender");
+            X.Add(30, "Engin");
+            X.Show();
+        }
+    }
+}
